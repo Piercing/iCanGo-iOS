d1ccf10c5f683e7a68e89ce437406b1483e7ecdf
“aortegas” <aortegas@icloud.com>
2016-07-12 14:04:13 +0200

    Images EndPoints

--- iCanGo-iOS/AppDelegate.swift
+++ iCanGo-iOS/AppDelegate.swift
@@ -18,11 +18,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // Navigation Bar Style
         UIApplication.sharedApplication().statusBarStyle = UIStatusBarStyle.LightContent
-             
+        
+        
         self.window?.rootViewController = StartUpController(nibName: nil, bundle: nil)
         self.window?.makeKeyAndVisible()
         return true
     }
 }
+
+
 
 
