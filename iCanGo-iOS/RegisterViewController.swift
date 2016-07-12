f5f2a0d4b797c8d2073a8c03fafe6954aca87a59
Piercing <merlosalbarracin@gmail.com>
2016-07-12 13:12:47 +0200

    MyProfile_UI_Edit finished

--- iCanGo-iOS/RegisterViewController.swift
+++ iCanGo-iOS/RegisterViewController.swift
@@ -4,6 +4,18 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    @IBOutlet weak var firstNameRegister: UITextField!
+    @IBOutlet weak var secondNameResgister: UITextField!
+    @IBOutlet weak var emailRegister: UITextField!
+    @IBOutlet weak var userRegister: UITextField!
+    @IBOutlet weak var passRegister: UITextField!
+    @IBOutlet weak var repeatPassRegister: UITextField!
+    @IBOutlet weak var cityRegister: UITextField!
+    @IBOutlet weak var countryRegister: UITextField!
+    
+    
     // MARK: - Init
     
     convenience init() {
@@ -22,4 +34,12 @@ class RegisterViewController: UIViewController {
         self.title = "Register"
         Appearance.customizeAppearance(self.view)
     }
+    
+    // MARK: - Actions
+    
+    @IBAction func saveRegister(sender: AnyObject) {
+        // TODO:
+        print("Tapped buttom register")
+    }
+    
 }
