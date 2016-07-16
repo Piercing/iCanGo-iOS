b8f8ccbff6dadfad032e9c8b23fc25de083885e6
Pedro Martín <pmargom@gmail.com>
2016-07-16 16:24:18 +0100

    CircularImage conflicts merged

--- iCanGo-iOS/Extensions.swift
+++ iCanGo-iOS/Extensions.swift
@@ -24,6 +24,36 @@ extension UIView {
     
 }
 
+extension UIImage {
+    
+    func circularImage(size size: CGSize?) -> UIImage {
+        let newSize = size ?? self.size
+        
+        let minEdge = min(newSize.height, newSize.width)
+        let size = CGSize(width: minEdge, height: minEdge)
+        
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        let context = UIGraphicsGetCurrentContext()
+        
+        self.drawInRect(CGRect(origin: CGPoint.zero, size: size), blendMode: .Copy, alpha: 1.0)
+        
+        CGContextSetBlendMode(context, .Copy)
+        CGContextSetFillColorWithColor(context, UIColor.clearColor().CGColor)
+        
+        let rectPath = UIBezierPath(rect: CGRect(origin: CGPoint.zero, size: size))
+        let circlePath = UIBezierPath(ovalInRect: CGRect(origin: CGPoint.zero, size: size))
+        rectPath.appendPath(circlePath)
+        rectPath.usesEvenOddFillRule = true
+        rectPath.fill()
+        
+        let result = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return result
+    }
+    
+}
+
 extension Double {
     
     var asLocaleCurrency:String {
